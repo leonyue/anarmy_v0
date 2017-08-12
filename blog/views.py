935fae87193a5f3845d4147e378aedf58094884d
得建岳 <dejianyue@dejianyuedeMacBook-Pro.local>
2017-08-12 12:28:32 +0800

    ajex refresh add to Index

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse,Strin
+from django.forms.models import model_to_dict
 
 from .models import BlogsPost
 
@@ -7,11 +9,32 @@ from django.views import generic
 
 app_name = 'blog'
 
+def countPlus(request):
+    if 'count' in request.session:
+        request.session['count'] += 1
+    else:
+        request.session['count'] = 1
+    return request.session['count']
+
+def resetCount(request):
+    if 'count' in request.session:
+        del request.session['count']
+
+def moreposts(request):
+    posts = BlogsPost.objects.order_by('-timestamp')[:2]
+    jsonPosts = []
+    for post in posts:
+        jsonPost = model_to_dict(post)
+        jsonPost['pk'] = post.pk
+        jsonPosts.append(jsonPost)
+    return JsonResponse(jsonPosts,safe=False)
+
 class Index(generic.ListView):
     context_object_name = "posts"
     template_name = "blog/index.html"
     def get_queryset(self):
-        return BlogsPost.objects.order_by('-timestamp')[:20]
+        resetCount(request= self.request)
+        return BlogsPost.objects.order_by('-timestamp')[:2]
 
 class Post(generic.DetailView):
     model = BlogsPost
