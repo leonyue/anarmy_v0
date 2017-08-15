28e8adf496955f02a204691518804ff68607a8eb
dj.yue <dj.yue@yuneec.com>
2017-08-15 11:19:36 +0800

    修改admin页顺序

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date','question_text']
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
